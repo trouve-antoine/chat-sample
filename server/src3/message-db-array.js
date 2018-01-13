8ac0d6be51e7c836ca85e7dc5d36141f97fe855f
Antoine Trouvé <trouve@aibod.com>
2018-01-14 06:22:33 +0900

    Remove the di from array-db too

--- server/src3/message-db-array.js
+++ server/src3/message-db-array.js
@@ -1,10 +1,8 @@
-module.exports = config => services => {
-  const allMessages = []
+allMessages = []
 
-  return {
-    appendMessage: message => Promise.resolve( allMessages.push(message) ),
-    getAllMessages: () => Promise.resolve( [...allMessages] ),
-    onRecieveAllMessages: () => {},
-    close: () => Promise.resolve()
-  }
+module.exports = {
+  appendMessage: message => Promise.resolve( allMessages.push(message) ),
+  getAllMessages: () => Promise.resolve( [...allMessages] ),
+  onRecieveAllMessages: () => {},
+  close: () => Promise.resolve()
 }
