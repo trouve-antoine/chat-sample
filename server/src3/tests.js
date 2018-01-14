70f85a24582620384042a6767a07c1b0065b2436
Antoine Trouvé <trouve@aibod.com>
2018-01-15 06:03:35 +0900

    fix the test

--- server/src3/tests.js
+++ server/src3/tests.js
@@ -1,55 +1,17 @@
 const assert = require('assert')
 
-const config = {
-  socketName: "chat", socketPath: "/chat-test",
-  antoineHost: 'localhost:4546' // TODO: adapt the IP
-}
-
-const log = {
-  info: () => {},
-  warn: console.warn,
-  error: console.error
-}
-
 describe("Test the array message DB", () => {
-  const services = { log }
-  const messageDb = require('./message-db-array')(config)(services)
-  messageDbTests(messageDb)
-})
+  const messageDb = require('./message-db-array')
 
-describe("Test the server message DB", () => {
-  const services = { log }
-  const messageDb = require('./message-db-server')(config)(services)
-  messageDbTests(messageDb)
-})
-
-function messageDbTests(messageDb) {
-  it("There are no message at the begining", () =>
-    messageDb.getAllMessages()
-    .then(allMessages => {
-      assert.equal(allMessages.length, 0)
-    })
-  )
-
-  it("Add a message", async () =>
-    messageDb.appendMessage("こんにちは")
-    .then(() =>
-      messageDb.getAllMessages()
-    )
-    .then(allMessages => {
-      assert.equal(allMessages.length, 1)
-      assert.equal(allMessages[0], "こんにちは")
-    })
-  )
-
-  it("Add another message", () => {
-    /* TODO */
-    /* 1. append the message */
-    /* 2. check there are now 2 messages */
-    /* 3. check the 2nd message is as expected */
+  it("There are no message at the begining", async () => {
+    const messages = await messageDb.getAllMessages()
+    assert.equal(messages.length, 0)
   })
 
-  it("Close the DB", () =>
-    messageDb.close()
-  )
-}
+  it("Add a message", async () => {
+    await messageDb.appendMessage("こんにちは")
+    const messages = await messageDb.getAllMessages()
+    assert.equal(messages.length, 1)
+    assert.equal(messages, "こんにちは")
+  })
+})
