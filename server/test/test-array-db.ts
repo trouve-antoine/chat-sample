5674c04e9cbd3fda5bc67d407fbf3ff8fe32fd48
Antoine Trouvé <trouve@aibod.com>
2018-09-12 05:56:50 +0900

    move tests around. Complete the tests of the array

---
+++ server/test/test-array-db.ts
@@ -0,0 +1,32 @@
+import { ArrayMessageDb } from "../src/message-db/array-db";
+
+const assert = require('assert')
+
+describe("Test the message DB", () => {
+  const messageDb = new ArrayMessageDb();
+
+  it("There are no message at the begining", async () => {
+    const allMessages = await messageDb.getAllMessages();
+    assert.equal( allMessages.length, 0)
+  })
+
+  it("Add a message", async () => {
+    messageDb.appendMessage("こんにちは")
+    const allMessages = await messageDb.getAllMessages();
+    assert.equal(allMessages.length, 1)
+    assert.equal(allMessages[0], "こんにちは")
+  })
+
+  it("Add another message", async () => {
+    messageDb.appendMessage("ヤッホー")
+    const allMessages = await messageDb.getAllMessages();
+    assert.equal(allMessages.length, 2)
+  })
+
+  it("Messages are the oldest first", async () => {
+    const allMessages = await messageDb.getAllMessages();
+    assert.equal(allMessages.length, 2)
+    assert.equal(allMessages[0], "こんにちは")
+    assert.equal(allMessages[1], "ヤッホー")
+  })
+})
